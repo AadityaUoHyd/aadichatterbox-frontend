4d21837e49e795ee6c799da9f804e8f187798862
Aaditya Bachchu Chatterjee <57300089+AadityaUoHyd@users.noreply.github.com>
2024-10-12 12:38:52 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   keycloak: {
-    url: "https://lemur-16.cloud-iam.com/auth",
-    realm: "aadichatterbox-keycloak",
+    url: "https://lemur-18.cloud-iam.com/auth",
+    realm: "aadichatterbox",
     clientId: "aadichatterbox"
   },
   API_URL: "https://aadichatterbox.netlify.app/api",
